5d1280bb4fa01b5ab19f035b58e74129f11e2aaa
Tarek Abdelmeguid <tarek.a@microsoft.com>
2022-11-21 12:57:59 -0800

    intellisense steps in VS Code

--- SparkGym/my-spark-apps/WordCount/SparkWordCount.scala
+++ SparkGym/my-spark-apps/WordCount/SparkWordCount.scala
@@ -1,3 +1,10 @@
+// IDE Integration: How to use intellisense: 
+//    1. Install Metals
+//    2. Open dir containing build.sbt file
+//    3. From the menubar, run View > Command Palette… (Cmd-Shift-P on macOS) “Metals: Switch build server”, and select “sbt”
+//    4. Once the import process is complete, open a Scala file to see that code completion works.
+//    Reference: https://www.scala-sbt.org/1.x/docs/IDE.html
+
 import org.apache.spark.{SparkContext, SparkConf}
 
 object SparkWordCount {
